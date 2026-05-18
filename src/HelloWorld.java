3c78b83c393cecb947809ab4b41144d156c01e3f
CHEN Yvon <12505979@s30411.iutv.univ-paris13.fr>
2026-05-18 15:50:44 +0200

    changement de print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Ciao mondo !");
-		System.out.println("Hello World !");
+		System.out.println("Hallo Welt !");
 		System.out.println("Hola Mundo !");
 	}
 
